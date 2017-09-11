0f58360830c9c15e0aba5fcf39cda8833a75b624
david20jazz <david20jazz@gmail.com>
2017-09-11 17:51:10 +0900

    Second Commit

--- app/src/main/java/xyz/bizforyou/mylistview/MainActivity.java
+++ app/src/main/java/xyz/bizforyou/mylistview/MainActivity.java
@@ -2,6 +2,8 @@ package xyz.bizforyou.mylistview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private List<String> mData;
+    private ArrayAdapter<String> mAdapter; // Adapter 준비
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +23,32 @@ public class MainActivity extends AppCompatActivity {
         initData();
 
         // 2. Adapter 준비
-
+        initAdapter();
 
         // 3. ListView 에 Adapter 장착
+        initListView();
 
+    }
+    // ListView 에 adapter 설정
+    private void initListView() {
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(mAdapter);
+    }
 
+    private void initAdapter() {
+        mAdapter = new ArrayAdapter<String>(getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                mData);
     }
 
     private void initData() {
-        mData = new ArrayList<>();
+        mData = new ArrayList<String>();
+        // 임의의 데이터 100개 추가
+        for(int i = 1; i <= 100; i++) {
+            mData.add("Item " + i);
+        }
     }
+
+
 
 }
